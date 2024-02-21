868eb500b3d83464d540e13a97590b3ad0b7a714
NirmalKumar M <nirmal@ideassion.com>
2024-02-21 12:46:42 +0530

    Authenticate api modified and Role column added in login

---
+++ EmployeeOnboarding/Migrations/AlterloginTable_20240221100000.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace EmployeeOnboarding.Migrations
+{
+    [Migration(20240221100000)]
+    public class AlterloginTable_20240221100000 : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Login")
+           .AddColumn("Role").AsString(30).Nullable();
+        }
+    }
+}
