cc58520f2b993e4e9028f3dc737588dcc19bc157
NirmalKumar M <nirmal@ideassion.com>
2024-02-23 01:05:53 +0530

    master api

--- EmployeeOnboarding/Controllers/UserDetailsController.cs
+++ EmployeeOnboarding/Controllers/UserDetailsController.cs
@@ -1,11 +1,14 @@
 ﻿using EmployeeOnboarding.Contracts;
 using EmployeeOnboarding.Data;
+using EmployeeOnboarding.Data.Enum;
 using EmployeeOnboarding.Request;
 using EmployeeOnboarding.Response;
 using EmployeeOnboarding.Services;
 using EmployeeOnboarding.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using OnboardingWebsite.Models;
+using System.ComponentModel;
+using System.Reflection;
 //using OnboardingWebsite.Models;
 
 namespace EmployeeOnboarding.Controllers
@@ -24,7 +27,7 @@ namespace EmployeeOnboarding.Controllers
 
 
         [HttpPost("AddPersonalInfo")]
-        public async Task<IActionResult> AddPersonalInfo([FromBody]PersonalInfoRequest personalInfoRequest)
+        public async Task<IActionResult> AddPersonalInfo([FromBody] PersonalInfoRequest personalInfoRequest)
         {
             var response = _userDetailsRepository.AddPersonalInfo(personalInfoRequest);
             if (response != null)
@@ -50,9 +53,63 @@ namespace EmployeeOnboarding.Controllers
                 return NoContent();
             }
         }
+        [HttpGet("Gender")]
+        public IActionResult Gender()
+        {
+            var enumValues = Enum.GetValues(typeof(Gender));
+            var enumDictionary = new Dictionary<string, int>();
+
+            foreach (var value in enumValues)
+            {
+                var name = Enum.GetName(typeof(Gender), value);
+                var id = (int)value;
+                enumDictionary.Add(name, id);
+            }
+
+            return Ok(enumDictionary);
+
+        }
+        [HttpGet("MartialStatus")]
+        public IActionResult MartialStatus()
+        {
+            var enumValues = Enum.GetValues(typeof(MartialStatus));
+            var enumDictionary = new Dictionary<string, int>();
+
+            foreach (var value in enumValues)
+            {
+                var name = Enum.GetName(typeof(MartialStatus), value);
+                var id = (int)value;
+                enumDictionary.Add(name, id);
+            }
+
+            return Ok(enumDictionary);
+        }
+        [HttpGet("BloodGroup")]
+        public IActionResult BloodGroup()
+        {
+
+            var enumValues = Enum.GetValues(typeof(BloodGroup));
+            var enumDictionary = new Dictionary<string, int>();
+
+            foreach (var value in enumValues)
+            {
+                var enumType = value.GetType();
+                var enumName = Enum.GetName(enumType, value);
+                var memberInfo = enumType.GetMember(enumName)[0];
+                var customAttribute = memberInfo.GetCustomAttribute<DescriptionAttribute>();
+
+                if (customAttribute != null)
+                {
+                    var customDescription = customAttribute.Description;
+                    var id = (int)value;
+                    enumDictionary.Add(customDescription, id);
+                }
+            }
+
+            return Ok(enumDictionary);
+        }
     }
 }
-       
 
         
 
